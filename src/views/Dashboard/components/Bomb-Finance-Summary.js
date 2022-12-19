f09b53ab13a496a2bff62d96725d5a917b2c30df
shubhra92 <106535306+shubhra92@users.noreply.github.com>
2022-12-19 14:38:15 +0000

    Pending changes exported from your codespace

--- src/views/Dashboard/components/Bomb-Finance-Summary.js
+++ src/views/Dashboard/components/Bomb-Finance-Summary.js
@@ -1,6 +1,8 @@
 import React, { useMemo } from "react";
 import {roundAndFormatNumber} from "../../../0x"
 import useBombStats from '../../../hooks/useBombStats';
+import usebShareStats from '../../../hooks/usebShareStats';
+import useBondStats from '../../../hooks/useBondStats';
 import TokenSymbol from "../../../components/TokenSymbol"
 import { Typography } from "@material-ui/core";
 import useCurrentEpoch from '../../../hooks/useCurrentEpoch';
@@ -9,16 +11,72 @@ import moment from 'moment';
 import useTreasuryAllocationTimes from '../../../hooks/useTreasuryAllocationTimes';
 import CountUp from 'react-countup';
 import useTotalValueLocked from '../../../hooks/useTotalValueLocked';
+import bombImg from "../../../assets/img/bomb-256.png";
+import bshareImg from "../../../assets/img/bshare-256.png";
+import bbondImg from "../../../assets/img/bbond-256.png";
+import MetamaskFox from '../../../assets/img/metamask-fox.svg';
+
 
 
 
 const BombFinanceSummary = () => {
+    const totalSupplyFunc = (value) =>{
+        if( !Number(value) ) return value;
+        if( value > (1000*1000)) value = [String(value/(1000*1000)) , "m"]
+        else if( value > 1000 ) value = [String(value/1000) ,'k'];
+        // return value
+        if( value[1]!==undefined ) {
+            if( /^(?=.*[.]).*$/.test(value) ) value[0]=value[0].split(".")
+            return (Array.isArray(value[0])?value[0][0]+"."+value[0][1][0]:value[0]) + value[1]
+        }else return value
+    }
     const TVL = useTotalValueLocked();
     const currentEpoch = useCurrentEpoch()
     const tokens = ["Bomb"]
+    
     const bombStats = useBombStats();
-    const bombTotalSupply = useMemo(() => (bombStats ? String(bombStats.totalSupply) : null), [bombStats]);
+    const bombTotalSupply = useMemo(() => (bombStats ? totalSupplyFunc(bombStats.totalSupply) : '--.--'), [bombStats]);
+    const bShareStats = usebShareStats();
+    const bShareTotalSupply = useMemo(() => (bShareStats ? totalSupplyFunc(bShareStats.totalSupply) : '--.--'), [bShareStats]);
+    const tBondStats = useBondStats();
+    const tBondTotalSupply = useMemo(() => (tBondStats ? totalSupplyFunc(tBondStats.totalSupply) : '--.--'), [tBondStats]);
+
+    const bombPriceInDollars = useMemo(
+        () => (bombStats ? Number(bombStats.priceInDollars).toFixed(2) : null),
+        [bombStats],
+      );
+    const bSharePriceInDollars = useMemo(
+        () => (bShareStats ? Number(bShareStats.priceInDollars).toFixed(2) : null),
+        [bShareStats],
+      );
+    const tBondPriceInDollars = useMemo(
+        () => (tBondStats ? Number(tBondStats.priceInDollars).toFixed(2) : null),
+        [tBondStats],
+      );
+
+
     const { to } = useTreasuryAllocationTimes();
+
+    const Cs_Ts_P_Table_Box = ({Top, LogoLabel, Logo, TotalSupply, DollarPrice}) => {
+        return (
+            <div className="cs--ts--p--table--box--3" style={{top: Top}} >
+                    <div style={{position:"relative"}}>
+                        <img className="logo--bbond--logo"  src={Logo} alt="" / >
+                        <label className="logo--bbond--label">${LogoLabel}</label>
+                    </div>
+                    <div className="cs--ts--p--value" >
+                        <label className="cs--value" >20.00K</label>
+                        <label className="ts--value">{TotalSupply}</label>
+                        <span className="p--value" >
+                            <label >${DollarPrice}</label>
+                            <label>1.15 BTCB</label>
+                        </span>
+                        <img className="metamaskfox--logo" src={MetamaskFox} alt="" />
+                    </div>
+                    <hr className="cs--ts--p--table--hr--2"/>
+            </div>
+        )
+    }
 
     return (
         <div className="summry--container">
@@ -39,10 +97,14 @@ const BombFinanceSummary = () => {
                 <p className="tvl">TVL: <CountUp style={{color:"rgba(0, 232, 162, 1)"}} end={TVL} separator="," prefix="$" /></p>
                 <p className="last--epoch--twap">Last Epoch TWAP: <span style={{color:"rgba(0, 232, 162, 1)"}}>1.22</span></p>
             </div>
-            <div className="cs--ts--p--table" style={{border:"2px solid red"}}>
-                <div className="cs--ts--p--table--box--3">
-                    
-                </div>
+            <div className="cs--ts--p--table" >
+                <label className="cs--label">Current Supply</label>
+                <label className="ts--label">Total Supply</label>
+                <label className="p--label">Price</label>
+                <hr className="cs--ts--p--table--hr--1"/>
+                <Cs_Ts_P_Table_Box Top={'30px'} LogoLabel={'BOMB'} Logo={bombImg} TotalSupply={bombTotalSupply} DollarPrice={bombPriceInDollars}/>
+                <Cs_Ts_P_Table_Box Top={'90px'} LogoLabel={'BSHARE'} Logo={bshareImg} TotalSupply={bShareTotalSupply} DollarPrice={bSharePriceInDollars}/>
+                <Cs_Ts_P_Table_Box Top={'150px'} LogoLabel={'BBOND'} Logo={bbondImg} TotalSupply={tBondTotalSupply} DollarPrice={tBondPriceInDollars}/>
             </div>
         </div>
     )
--- src/views/Dashboard/components/ReadIS.js
+++ src/views/Dashboard/components/ReadIS.js
@@ -3,7 +3,9 @@ import React from "react";
 const ReadIS = () => {
     return (
         <div className="read--I--S">
-            <a src="" style={{margin:"0"}}>Read Investment Strategy &#8250;</a>
+            <a src="">Read Investment Strategy </a>
+            <b style={{position:'absolute',margin:0,lineHight:"0",fontSize:"25px",fontWeight:"50",top:'-4px',left:'242px',color:"#9EE6FF"}}>&#8250;</b>
+            
         </div>
     )
 }
